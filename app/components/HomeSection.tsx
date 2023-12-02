f42ffcf2ef94a4be2743827f5b7223418d3aa176
HoangCoder <93141009+NicolasWillyam@users.noreply.github.com>
2023-12-03 06:57:30 +0700

    Update frame-motion

--- app/components/HomeSection.tsx
+++ app/components/HomeSection.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
@@ -7,74 +7,85 @@ import card1 from "../../public/card1.jpeg";
 import card2 from "../../public/card2.jpeg";
 import card3 from "../../public/card3.jpeg";
 import card4 from "../../public/card4.jpeg";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { slideInFromLeft, slideInFromRight, slideInFromTop } from "@/ultis/motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/ultis/motion";
+
+const objectVariants = {
+  initial: {
+    x: 500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 2,
+      stggerChildren: 0.1,
+    },
+  },
+};
 
 export default function HomeSection() {
   return (
     <motion.div
-    initial="hidden"
-    animate="visible"   
-    className="w-full h-auto pb-20 relative">
+      initial="hidden"
+      animate="visible"
+      className="w-full h-auto pb-20 relative"
+    >
       <div className="grid sm:grid-cols-2 ">
-      <div className=" content section items-center flex-row justify-center mt-24">
-        <motion.div
-          variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
-        >
-          <span>
-            Providing
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              {" "}
-              the best{" "}
+        <div className=" content section items-center flex-row justify-center mt-24">
+          <motion.div
+            variants={slideInFromLeft(0.5)}
+            className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+          >
+            <span>
+              Providing
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+                {" "}
+                the best{" "}
+              </span>
+              Finance project
             </span>
-             Finance project
-          </span>
-        </motion.div>
-         
-        <motion.div
-          variants={slideInFromLeft(0.75)} className="mt-8 text-gray-400">
+          </motion.div>
+
+          <motion.div
+            variants={slideInFromLeft(0.75)}
+            className="mt-8 text-gray-400"
+          >
             Empowering on-chain liquidity, governance, and cash flow for DeFi
             protocols
           </motion.div>
-          <motion.div
-          variants={slideInFromLeft(0.75)}>
+          <motion.div variants={slideInFromLeft(0.75)}>
             <button className="bg-white w-full sm:w-fit mt-8 text-black px-9 py-3 text-[16px] font-medium rounded-full">
-            Learn More
-          </button>
+              Learn More
+            </button>
           </motion.div>
-          
         </div>
         <motion.div
-          variants={slideInFromRight(0.75)}className="sm:flex hidden items-center justify-center -z-10">
-        <video autoPlay loop className="w-[420px] h-[420px]">
-        <source src="/spin.webm" />
-      </video>
+          variants={slideInFromRight(0.75)}
+          className="sm:flex hidden items-center justify-center -z-10"
+        >
+          <video autoPlay loop className="w-[420px] h-[420px]">
+            <source src="/spin.webm" />
+          </video>
         </motion.div>
       </div>
 
-      <div className="mt-12 grid sm:grid-cols-4 grid-cols-2 sm:gap-12 gap-4">
       <motion.div
-          variants={slideInFromRight(1.5)}>
-            <ProjectCard name="Redacted" imgUrl={card1} />
-          </motion.div>
-
-          <motion.div
-          variants={slideInFromRight(1.25)}>
-            <ProjectCard name="Pirex" imgUrl={card2} />
-          </motion.div>
-
-          <motion.div
-          variants={slideInFromRight(1)}>
-            <ProjectCard name="Hidden Hand" imgUrl={card3} />
-          </motion.div>
-
-          <motion.div
-          variants={slideInFromRight(0.75)}>
-            <ProjectCard name="Dinero" imgUrl={card4} />
-          </motion.div>
-  
-      </div>
+        variants={objectVariants}
+        initial="initial"
+        animate="animate"
+        className="mt-12 grid sm:grid-cols-4 grid-cols-2 sm:gap-12 gap-4"
+      >
+        <ProjectCard name="Redacted" imgUrl={card1} />
+        <ProjectCard name="Pirex" imgUrl={card2} />
+        <ProjectCard name="Hidden Hand" imgUrl={card3} />
+        <ProjectCard name="Dinero" imgUrl={card4} />
       </motion.div>
+    </motion.div>
   );
 }
--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -2,6 +2,22 @@ import Image from "next/image";
 import React from "react";
 import bg from "../../public/card1.jpeg";
 import { CgMoreO } from "react-icons/cg";
+import { motion } from "framer-motion";
+
+const objectVariants = {
+  initial: {
+    x: 500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 2,
+      stggerChildren: 0.1,
+    },
+  },
+};
 
 export default function ProjectCard({
   name,
@@ -11,13 +27,12 @@ export default function ProjectCard({
   imgUrl: any;
 }) {
   return (
-    <div className=" rounded-2xl">
+    <motion.div variants={objectVariants} className=" rounded-2xl">
       <div
         style={{
           backgroundImage: `url(${imgUrl.src})`,
-          
         }}
-        className="bg-cover bg-no-repeat bg-center rounded-t-2xl pt-4 w-full sm:h-[320px] h-[240px]"
+        className="bg-cover relative bg-no-repeat bg-center rounded-2xl pt-4 w-full sm:h-[360px] h-[280px]"
       >
         <div className="w-fit bg-gray-900/75 text-white px-3 py-2 pr-6 rounded-r-full relative">
           <div className="flex items-center">
@@ -33,16 +48,12 @@ export default function ProjectCard({
             <p className="text-[14px] font-normal ml-2 text-gray-200">{name}</p>
           </div>
         </div>
+        <div className="flex text-[14px] w-full px-3 absolute bottom-3">
+          <button className="bg-white text-black  w-full font-medium py-3 rounded-full transition duration-700 ease-in-out hover:bg-gray-800/75 hover:text-white">
+            Launch App
+          </button>
+        </div>
       </div>
-      <div className="flex text-[14px] w-full">
-        <button className="bg-white text-black  w-full font-medium py-3 rounded-bl-2xl transition duration-700 ease-in-out hover:bg-gray-800/75 hover:text-white">
-          Launch App
-        </button>
-
-        <button className="bg-gray-800/75 w-16  text-white font-medium py-3 px-2 text-[12px] rounded-br-2xl flex items-center justify-center transition duration-700 ease-in-out hover:text-gray-800/75 hover:bg-white">
-          <CgMoreO size={16} />
-        </button>
-      </div>
-    </div>
+    </motion.div>
   );
 }
