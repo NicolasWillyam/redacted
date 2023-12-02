548d4f8ed414f704145cae6406f9c196ffd1f51a
NicolasWylliam <phambahuyhoang0@gmail.com>
2023-12-03 04:55:04 +0700

    Add framer-motion and background star

--- app/components/HomeSection.tsx
+++ app/components/HomeSection.tsx
@@ -1,43 +1,80 @@
+"use client"
 import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
 import ProjectCard from "./ProjectCard";
 import card1 from "../../public/card1.jpeg";
 import card2 from "../../public/card2.jpeg";
 import card3 from "../../public/card3.jpeg";
 import card4 from "../../public/card4.jpeg";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from "@/ultis/motion";
 
 export default function HomeSection() {
   return (
-    <div className="w-full h-auto pb-20 relative">
-      <div className="grid sm:grid-cols-2">
-        <div className="sm:w-3/4 content section items-center flex-row justify-center mt-32">
-          <p className="text-4xl font-bold text-white">REDACTED FINANCE</p>
-          <p className="mt-8 text-gray-400">
+    <motion.div
+    initial="hidden"
+    animate="visible"   
+    className="w-full h-auto pb-20 relative">
+      <div className="grid sm:grid-cols-2 ">
+      <div className=" content section items-center flex-row justify-center mt-24">
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Providing
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              the best{" "}
+            </span>
+             Finance project
+          </span>
+        </motion.div>
+         
+        <motion.div
+          variants={slideInFromLeft(0.75)} className="mt-8 text-gray-400">
             Empowering on-chain liquidity, governance, and cash flow for DeFi
             protocols
-          </p>
-          <button className="bg-white w-full sm:w-fit mt-8 text-black px-9 py-3 text-[16px] font-medium rounded-full">
+          </motion.div>
+          <motion.div
+          variants={slideInFromLeft(0.75)}>
+            <button className="bg-white w-full sm:w-fit mt-8 text-black px-9 py-3 text-[16px] font-medium rounded-full">
             Learn More
           </button>
+          </motion.div>
+          
         </div>
-        <div className="sm:flex hidden -mt-[160px] sm:-ml-[80px] sm:w-[720px] sm:h-[720px] w-[360] h-[360px] bg-blue-500 rounded-full pt-20 blur items-center justify-center -z-10">
-          <Image
-            src="/logo.png"
-            width={420}
-            height={420}
-            alt="image logo"
-            className="animate-bounce"
-          />
-        </div>
+        <motion.div
+          variants={slideInFromRight(0.75)}className="sm:flex hidden items-center justify-center -z-10">
+        <video autoPlay loop className="w-[420px] h-[420px]">
+        <source src="/spin.webm" />
+      </video>
+        </motion.div>
       </div>
 
-      <div className="mt-32 sm:-mt-32 grid sm:grid-cols-4 grid-cols-2 sm:gap-12 gap-4">
-        <ProjectCard name="Redacted Protocol" imgUrl={card1} />
-        <ProjectCard name="Pirex" imgUrl={card2} />
-        <ProjectCard name="Hidden Hand" imgUrl={card3} />
-        <ProjectCard name="Dinero" imgUrl={card4} />
+      <div className="mt-12 grid sm:grid-cols-4 grid-cols-2 sm:gap-12 gap-4">
+      <motion.div
+          variants={slideInFromRight(1.5)}>
+            <ProjectCard name="Redacted" imgUrl={card1} />
+          </motion.div>
+
+          <motion.div
+          variants={slideInFromRight(1.25)}>
+            <ProjectCard name="Pirex" imgUrl={card2} />
+          </motion.div>
+
+          <motion.div
+          variants={slideInFromRight(1)}>
+            <ProjectCard name="Hidden Hand" imgUrl={card3} />
+          </motion.div>
+
+          <motion.div
+          variants={slideInFromRight(0.75)}>
+            <ProjectCard name="Dinero" imgUrl={card4} />
+          </motion.div>
+  
       </div>
-    </div>
+      </motion.div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import StarsCanvas from "./components/StarBackground";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-      <body style={{background: '#000'}} className={inter.className}>{children}</body>
+      
+      <body style={{background: '#000'}} className={inter.className}>
+       
+        {children} <StarsCanvas /></body>
     </html>
   );
 }
